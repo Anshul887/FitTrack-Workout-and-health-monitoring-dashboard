96aa031f50d297565d7d6e563b03e771e4d26ff5
Anshul887 <anshulmankar887@gmail.com>
2026-05-27 14:28:42 +0530

    Create authMiddleware.ts

---
+++ src/middleware/authMiddleware.ts
@@ -0,0 +1,37 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
+
+export interface AuthRequest extends Request {
+  user?: any;
+}
+
+const authMiddleware = (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.status(401).json({
+      message: 'Unauthorized'
+    });
+  }
+
+  try {
+    const decoded = jwt.verify(
+      token.replace('Bearer ', ''),
+      process.env.JWT_SECRET!
+    );
+
+    req.user = decoded;
+
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      message: 'Invalid token'
+    });
+  }
+};
+
+export default authMiddleware;
