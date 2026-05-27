dfab4b916cbeac0719cc680071401a04c75af178
Anshul887 <anshulmankar887@gmail.com>
2026-05-27 14:27:18 +0530

    Create Meal.ts

---
+++ src/models/Meal.ts
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+const mealSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    },
+
+    title: String,
+
+    calories: Number,
+
+    protein: Number,
+
+    carbs: Number,
+
+    fats: Number
+  },
+  {
+    timestamps: true
+  }
+);
+
+export default mongoose.model('Meal', mealSchema);
